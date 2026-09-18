d3b8a879188fccf64add686ac935413cf3e82c71
Karla Angelica Rojano <l23240010@smartin.tecnm.mx>
2026-09-17 23:00:48 -0600

    camb

--- Vista/include/header.php
+++ Vista/include/header.php
@@ -28,7 +28,7 @@
         </li>
 
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="consultarhis.php">Historiales</a>
+          <a class="nav-link active" aria-current="page" href="buscarhist.php">Historiales</a>
         </li>
 
         <li class="nav-item">
